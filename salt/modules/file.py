a1cd8ffa5a32460afdf567ff9443f3aeed7e3c43
Erik Johnson <palehose@gmail.com>
2014-11-05 14:19:25 -0600

    Use a dict instead of a list of lists for HASHES

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -57,14 +57,14 @@ __func_alias__ = {
     'makedirs_': 'makedirs'
 }
 
-HASHES = [
-            ['sha512', 128],
-            ['sha384', 96],
-            ['sha256', 64],
-            ['sha224', 56],
-            ['sha1', 40],
-            ['md5', 32],
-         ]
+HASHES = {
+    'sha512': 128,
+    'sha384': 96,
+    'sha256': 64,
+    'sha224': 56,
+    'sha1': 40,
+    'md5': 32,
+}
 
 
 def __virtual__():
@@ -2678,7 +2678,6 @@ def get_managed(
                         return '', {}, ('Source hash file {0} contains an invalid '
                             'hash format, it must be in the format <hash type>=<hash>.'
                             ).format(source_hash)
-
                 else:
                     # The source_hash is a hash string
                     comps = source_hash.split('=')
