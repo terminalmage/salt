f3810bd5a4528f62466164269d55818b7ff6c62b
Erik Johnson <palehose@gmail.com>
2017-06-26 21:40:44 -0500

    Introduce "priority" feature for pyobjects
    When there are multiple matches (such as when both an os_family and os
    grain both match the minion), the order in which they are processed is
    determined by the iteration order of the class' __dict__ attribute.
    Since this would be different in different versions of Python, behavior
    would be inconsistent depending on which version of Python is being used.
    
    This adds a priority attribute to the Map subclass which, if present,
    will re-order the match groups such that the results will be predictable
    irrespective of Python version.

--- salt/utils/pyobjects.py
+++ salt/utils/pyobjects.py
@@ -6,12 +6,14 @@ Pythonic object interface to creating state data, see the pyobjects renderer
 for more documentation.
 '''
 from __future__ import absolute_import
+import collections
 import inspect
 import logging
 
 from salt.utils.odict import OrderedDict
 from salt.utils.schema import Prepareable
 import salt.ext.six as six
+from salt.ext.six.moves import range  # pylint: disable=import-error,redefined-builtin
 
 REQUISITES = ('listen', 'onchanges', 'onfail', 'require', 'watch', 'use', 'listen_in', 'onchanges_in', 'onfail_in', 'require_in', 'watch_in', 'use_in')
 
@@ -330,10 +332,7 @@ class MapMeta(six.with_metaclass(Prepareable, type)):
             # if so use it, otherwise use the name of the object
             # this is so that you can match complex values, which the python
             # class name syntax does not allow
-            if hasattr(filt, '__match__'):
-                match = filt.__match__
-            else:
-                match = item
+            match = getattr(filt, '__match__', item)
 
             match_attrs = {}
             for name in filt.__dict__:
@@ -341,6 +340,35 @@ class MapMeta(six.with_metaclass(Prepareable, type)):
                     match_attrs[name] = filt.__dict__[name]
 
             match_info.append((grain, match, match_attrs))
+
+        # Reorder based on priority
+        try:
+            if not hasattr(cls.priority, '__iter__'):
+                log.error('pyobjects: priority must be an iterable')
+            else:
+                new_match_info = []
+                for grain in cls.priority:
+                    if not isinstance(grain, collections.Hashable):
+                        log.error('pyobjects: %s is not hashable', grain)
+                    else:
+                        # Using list() here because we will be modifying
+                        # match_info during iteration
+                        for index, item in list(enumerate(match_info)):
+                            try:
+                                if item[0] == grain:
+                                    # Add item to new list
+                                    new_match_info.append(item)
+                                    # Clear item from old list
+                                    match_info[index] = None
+                            except TypeError:
+                                # Already moved this item to new list
+                                pass
+                # Add in any remaining items not defined in priority
+                new_match_info.extend([x for x in match_info if x is not None])
+                # Save reordered list as the match_info list
+                match_info = new_match_info
+        except AttributeError:
+            pass
 
         # Check for matches and update the attrs dict accordingly
         attrs = {}
